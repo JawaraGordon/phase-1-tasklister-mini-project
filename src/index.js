de0529757373639ac192813ce9f357c62ffca8e9
JawaraGordon <jawaragordon@icloud.com>
2022-04-02 22:44:40 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  //   As a user, I should be able to type a task into the input field.
+  const newDescription = document.getElementById("new-task-description");
+
+  const newTaskName = document.getElementById("create-task-form");
+
+  // As a user, I should be able to click some form of a submit button.
+
+  newTaskName.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = (e) => {
+  e.preventDefault();
+
+  const newDescription = document.getElementById("new-task-description");
+  const newTaskItem = document.createElement("li");
+  newTaskItem.innerText = newDescription.value;
+
+  appendNewTask(newTaskItem);
+  e.target.reset();
+};
+
+// As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+
+const appendNewTask = function (task) {
+  document.getElementById("tasks").appendChild(task);
+};
